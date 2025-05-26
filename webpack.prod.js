54f4d817491c57d742614690dcb7a4a87c92562b
JanuarBani <januarbani2201@gmail.com>
2025-05-26 17:50:51 +0800

    First deploy: update images & config

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,8 @@ const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -27,12 +27,17 @@ module.exports = merge(common, {
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      chunks: "all", // Memisahkan vendor dan kode yang shared
+    },
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
     new InjectManifest({
-      swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
-      swDest: 'sw.bundle.js',
+      swSrc: path.resolve(__dirname, "src/scripts/sw.js"),
+      swDest: "sw.bundle.js",
     }),
   ],
 });
